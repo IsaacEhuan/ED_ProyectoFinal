4a12074e6cb53a2f42b1c323cb1e18e838af7c16
Daniel Moreno <A20216399@alumnos.uady.mx>
2022-05-21 23:17:40 -0500

    Agregado importar archivo de egresados y mostrar contenido en tabla.

---
+++ EDatosNoLineales/src/Interfaz/ArchivoImp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Interfaz;
+//CLASE EN LA QUE SE OBTIENE INFORMACION DEL ARCHIVO IMPORTADO
+//ABIERTO A DARLE MAS USOS QUE EL ACTUAL
+/**
+ *
+ * @author donco
+ */
+public class ArchivoImp {
+    public static String rutaArchivo;
+    public ArchivoImp(){}
+    public ArchivoImp(String ruta){
+        rutaArchivo=ruta;
+    }
+    public String getRuta(){
+        return rutaArchivo;
+    }
+}
