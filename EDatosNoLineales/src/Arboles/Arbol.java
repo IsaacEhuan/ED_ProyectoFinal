8fdd9f39bfd6e124a2379de4a1cb51439c80168f
IsaacEhuan <isaacehuanavila64f@gmail.com>
2022-05-26 10:44:18 -0500

    Lógica de búsqueda y búsqueda de promedio si es igual a.

--- EDatosNoLineales/src/Arboles/Arbol.java
+++ EDatosNoLineales/src/Arboles/Arbol.java
@@ -5,6 +5,7 @@ package Arboles;
 import archivos.LectorArchivo;
 import archivos.VerificadorArchivo;
 import avl.*;
+import listas.*;
 
 public class Arbol {
     public static ArbolAVL ArbolNombres;
@@ -21,6 +22,7 @@ public class Arbol {
             ArbolCalificacion.insertarInt(actula.calificacion, i);
             ArbolNombres.insertarString(actula.nombre, i);
             ArbolProfesion.insertarString(actula.profesion,i);
+            
         }
     }
 
@@ -74,10 +76,57 @@ public class Arbol {
     
     }
 
-    public static void busquedaIndividual(int comparador, ArbolAVL arbol){
-        //Esta función puede filtrar el promedio
-      
-      }
+    //NOTA: 1 = Mayor que, 2 = Menor que, 3 = Igual a
+    public static ListaSimple busquedaIndividual(int comparador, int opcion, ArbolAVL arbol){
+        ListaSimple referencias = null;
+        boolean band=true;
+        NodoAVL nodoActual = ArbolCalificacion.raiz;
+        switch(opcion){
+            case 1:
+            while(band){
+                if((int)nodoActual.getDato() == comparador){
+                    referencias = nodoActual.getDuplicados();
+                    band=false;
+                } else{
+                    if((int)nodoActual.getDato() > comparador){
+                        nodoActual = (NodoAVL)nodoActual.getIzq();
+                    } else{
+                        nodoActual = (NodoAVL)nodoActual.getDer();
+                    }
+                }
+            }
+            break;
+            case 2:
+            while(band){
+                if((int)nodoActual.getDato() == comparador){
+                    referencias = nodoActual.getDuplicados();
+                    band=false;
+                } else{
+                    if((int)nodoActual.getDato() > comparador){
+                        nodoActual = (NodoAVL)nodoActual.getIzq();
+                    } else{
+                        nodoActual = (NodoAVL)nodoActual.getDer();
+                    }
+                }
+            }
+            break;
+            case 3:
+            while(band){
+                if((int)nodoActual.getDato() == comparador){
+                    referencias = nodoActual.getDuplicados();
+                    band=false;
+                } else{
+                    if((int)nodoActual.getDato() > comparador){
+                        nodoActual = (NodoAVL)nodoActual.getIzq();
+                    } else{
+                        nodoActual = (NodoAVL)nodoActual.getDer();
+                    }
+                }
+            }
+            break;
+        }
+        return referencias;
+    }
 
     public static void busquedaDoble(int comparador,String comparador2, ArbolAVL arbol){
         //Esta función puede filtrar el promedio y cualquier otro parametro (nombre o profesión)
@@ -87,6 +136,7 @@ public class Arbol {
         //Esta funcion solo puede filtrar nombre y promedio
       
       }  
+    //REVISIÓN: ¿DEBERÍA HABER BÚSQUEDA DE 3?
     public static void busquedaTriple(String comparador,String comparador2, int comparador3, ArbolAVL arbol){
         //Esta funcion filtra por los tres parametros
     }
@@ -106,7 +156,12 @@ public class Arbol {
         */
 
         //Como usar la clase
-        String nombreArchivo = "C:/Users/picar/Desktop/Software/Estructura de Datos/ProyectoFinal/ED_ProyectoFinal/EDatosNoLineales/src/Interfaz/Egresados.csv";
+        
+        //Ruta de Pat:
+        //String nombreArchivo = "C:/Users/picar/Desktop/Software/Estructura de Datos/ProyectoFinal/ED_ProyectoFinal/EDatosNoLineales/src/Interfaz/Egresados.csv";
+        
+        //Ruta de Isaac:
+        String nombreArchivo = "C:\\Users\\isaac\\Downloads\\ED_ProyectFinal\\ED_ProyectoFinal\\EDatosNoLineales\\src\\Interfaz\\Egresados.csv";
 
         try {
             obtenerInformacion(nombreArchivo);
@@ -116,12 +171,14 @@ public class Arbol {
 
         System.out.println("-Calificacione-");
         ArbolCalificacion.inOrden();
-        
+        System.out.println("Indices con 90: ");
+        busquedaIndividual(90, 3, ArbolCalificacion).imprimir();
+        //System.out.println(ArbolCalificacion.raiz.getDato());
         System.out.println("-Nombre-");
-        ArbolNombres.inOrden();
+        //ArbolNombres.inOrden();
 
         System.out.println("-Profesion-");
-        ArbolProfesion.inOrden();
+        //ArbolProfesion.inOrden();
 
 
 
